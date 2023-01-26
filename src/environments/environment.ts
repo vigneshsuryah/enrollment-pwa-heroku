09d3051ff2dac0130ba650e66a15e52b33d2c997
Vignesh Suryah M <vigneshsuryah.blue@gmail.com>
2023-01-25 21:53:15 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 export const environment = {
   production: false,
   version: "LOCAL",
-  main_api_url: "https://enrollment-service-cloud.herokuapp.com",
+  main_api_url: "https://enrollment-service-cloud.onrender.com",
   vapid_public_key: "BL8vx3ym0aZMrSr5nIMxw7rv3meD7AidviFiifbjG7fGJqeO72Nyzh7CaotVkhb4Lc3-WLZ0E-X5J-F-L5ZOecA"
 };
